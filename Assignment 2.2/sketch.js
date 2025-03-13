1be0fa60f46bc6c2c0c6256a1d1df4882c7d3261
Darrius Davidson <ddav218@lsu.edu>
2025-03-13 14:21:06 -0500

    Finished Assignment 2.2

--- Assignment 2.2/sketch.js
+++ Assignment 2.2/sketch.js
@@ -1,19 +1,41 @@
+//import * as Tone from 'tone';
+
 let synth1, filt, distortion, metalSynth, noise1, ampEnv1, filt1;
 let activeKey = null;
 let filterSlider;
 
 let keyNotes = {
-  'a': 'A4', 's': 'B4', 'd': 'C5', 'f': 'D5',
-  'g': 'E5', 'h': 'F5', 'j': 'G5', 'k': 'A5'
+  'a': 'A4', 
+  's': 'B4', 
+  'd': 'C5', 
+  'f': 'D5',
+  'g': 'E5', 
+  'h': 'F5', 
+  'j': 'G5', 
+  'k': 'A5'
 };
 
 let metalKeys = {
-  'q': 'C4', 'w': 'D4', 'e': 'F4', 'r': 'G4',
-  't': 'A4', 'y': 'B4', 'u': 'C5', 'i': 'D5'
+  'q': 'C4',
+  'w': 'D4', 
+  'e': 'F4', 
+  'r': 'G4',
+  't': 'A4', 
+  'y': 'B4', 
+  'u': 'C5', 
+  'i': 'D5'
 };
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+
+  let startButton = createButton("Start Audio");
+  startButton.position(20, 400);
+  startButton.mousePressed(() => {
+    Tone.start().then(() => {
+      console.log("Audio started!");
+    });
+  });
 
   filterSlider = createSlider(200, 5000, 1500, 1);
   filterSlider.position(20, 350);
@@ -47,6 +69,7 @@ function setup() {
 
 function draw() {
   background(220);
+ // fill("black");
   text("A-K: Synth Lead\nQ-I: Percussive Sounds\nZ: Noise Hit", 20, 20);
   text("Filter Adjust", 20, 340);
   filt.frequency.value = filterSlider.value();
@@ -58,12 +81,15 @@ function keyPressed() {
   let metalPitch = metalKeys[keyPressed];
 
   if (pitch && keyPressed !== activeKey) {
+   // console.log("Playing Synth:", pitch);
     synth1.triggerRelease();
     activeKey = keyPressed;
     synth1.triggerAttack(pitch);
   } else if (metalPitch) {
-    metalSynth.triggerAttackRelease('0.5');
+   // console.log("Playing Metal Synth:", metalPitch);
+    metalSynth.triggerAttackRelease(metalPitch, '0.5');
   } else if (keyPressed === "z") {
+   // console.log("Triggering Noise Hit");
     ampEnv1.triggerAttackRelease(0.1);
   }
 }
