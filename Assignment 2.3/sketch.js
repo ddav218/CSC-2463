c7627ddf8eefc023287df0aca92ce0d406ecafa9
Darrius Davidson <ddav218@lsu.edu>
2025-03-16 15:35:20 -0500

    Finished Assignment 2.3

--- Assignment 2.3/sketch.js
+++ Assignment 2.3/sketch.js
@@ -1,67 +1,66 @@
-let osc, noise, env, filter, lfo;
-let showImage = false;
-
-function preload() {
-  img = loadImage('assets/vault.jpg');
-}
+let waterGun;
+let shooting = false;
 
 function setup() {
-  createCanvas(800, 800);
-  osc = new p5.Oscillator('sawtooth');
-  noise = new p5.Noise('brown');
-  filter = new p5.BiquadFilter();
-  lfo = new p5.Oscillator('triangle');
-  env = new p5.Envelope();
-
-  filter.freq(400);
-  filter.res(8);
-  osc.disconnect();
-  osc.connect(filter);
-  noise.disconnect();
-  noise.connect(filter);
-  filter.connect(p5.soundOut);
-
-  env.setADSR(0.05, 0.2, 0.3, 0.5);
-  env.setRange(0.8, 0);
-  
-  lfo.freq(2);
-  lfo.amp(200);
-  lfo.disconnect();
-  lfo.connect(filter.frequency);
+    createCanvas(800, 800);
+    waterGun = loadImage("assets/waterGun.png"); 
 }
 
 function draw() {
-  background(50);
-  fill(255);
-  textAlign(CENTER, CENTER);
-  text("Click to turn the vault!", width / 2, height / 2);
-  
-  if (showImage) {
-    image(img, width / 2 - 50, height / 2 - 50, 100, 100);
-  }
+    
+    background(220);
+    textAlign(CENTER, TOP);
+    textSize(36);
+    fill(0);
+    text("Click the gun to splash water", 400, 75);
+    image(waterGun, 100, 150, 300, 150);
+    
+    if (shooting) {
+        drawWaterStream();
+    }
+}
+
+function drawWaterStream() {
+    stroke(0, 0, 255);
+    strokeWeight(4);
+    for (let i = 0; i < 5; i++) {
+        line(300, 215, 480 + random(-10, 10), 190 + random(-20, 20));
+    }
+    noStroke();
 }
 
 function mousePressed() {
-  showImage = true;
-  playSoundEffect();
-  setTimeout(() => showImage = false, 1000);
+    shooting = true;
+    playWaterGun();
+    setTimeout(() => shooting = false, 500);
 }
 
-function playSoundEffect() {
-  osc.freq(100);
-  osc.start();
-  noise.start();
-  lfo.start();
-  env.play(osc);
-  env.play(noise);
-  
-  setTimeout(() => {
-    osc.freq(60, 0.5);
-  }, 300);
-  
-  setTimeout(() => {
-    osc.stop();
-    noise.stop();
-    lfo.stop();
-  }, 1000);
+function playWaterGun() {
+    let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    let bufferSize = audioCtx.sampleRate * 0.2;
+    let buffer = audioCtx.createBuffer(1, bufferSize, audioCtx.sampleRate);
+    let output = buffer.getChannelData(0);
+    
+    for (let i = 0; i < bufferSize; i++) {
+        output[i] = (Math.random() * 2 - 1) * Math.exp(-i / bufferSize * 10);
+    }
+    
+    let noise = audioCtx.createBufferSource();
+    noise.buffer = buffer;
+    noise.loop = false;
+    
+    let filter = audioCtx.createBiquadFilter();
+    filter.type = "bandpass";
+    filter.frequency.setValueAtTime(1200, audioCtx.currentTime);
+    filter.frequency.exponentialRampToValueAtTime(600, audioCtx.currentTime + 0.2);
+    
+    let gainNode = audioCtx.createGain();
+    gainNode.gain.setValueAtTime(0.8, audioCtx.currentTime);
+    gainNode.gain.exponentialRampToValueAtTime(0.01, audioCtx.currentTime + 0.2);
+    
+    noise.connect(filter);
+    filter.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+    
+    noise.start();
 }
